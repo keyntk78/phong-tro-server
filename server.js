45fe6359b49a2b213c9617099e7d2b87703f4423
Nguyễn Tuấn Kiệt <101924064+keyntk78@users.noreply.github.com>
2023-05-05 18:31:14 +0700

    fix cors

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import initRoutes from './src/routes/index'
 import connectDb from './src/config/database'
 
 const app = express()
-app.use(cors({ origin: process.env.CLIENT_URL, methods: ['POST', 'GET', 'PUT', 'DELETE'] }))
+app.use(cors({ origin: '*', methods: ['POST', 'GET', 'PUT', 'DELETE'] }))
 
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true, limit: '10mb' }))
